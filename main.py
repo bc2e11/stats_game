b01287d3e3a753c950db03b6c00929a94da7e3d1
bc <bc2tothe11@gmail.com>
2022-02-07 09:03:15 -0500

    Finished -- period 2 | started to implement dmg and changed got rid of 'leader1'... 'leader3' infrastructure

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import random
 from enum import Enum
+leaders = ["leader1", "leader2", "leader3"]
 
-leader1 = "leader1"
-leader2 = "leader2"
-leader3 = "leader3"
-
-leaders = [leader1, leader2, leader3]
-
-class Leader:
+class Leader(Enum):
+    ONE =   "leader1"
+    TWO =   "leader2"
+    THREE = "leader3"
     def __init__(self, leader):
         self.leader = leader
 
     def getUnit(self, leaderName, n):
         units = []
 
-        if leaderName == leader1: units = ["leader1Melee", "leader1Ranged", "leader1Cavalry"]
-        if leaderName == leader2: units = ["leader2Melee", "leader2Ranged", "leader2Cavalry"]
-        if leaderName == leader3: units = ["leader3Melee", "leader3Ranged", "leader3Cavalry"]
+        if leaderName == Leader.ONE: units = ["leader1Melee", "leader1Ranged", "leader1Cavalry"]
+        if leaderName == Leader.TWO: units = ["leader2Melee", "leader2Ranged", "leader2Cavalry"]
+        if leaderName == Leader.THREE: units = ["leader3Melee", "leader3Ranged", "leader3Cavalry"]
         # Returns list with units available per leader
+        self.units = units
         return units[n]
-    
+
+class Units(Enum):
+    MELEE = 30 
+    RANGED = 25
+    CAVALRY = 40
  
-print("1){0}\n2){1}\n3){2}".format(leader1, leader2, leader3))
+for i in range(3):
+    print(str(i+1)+")", "{0}".format(leaders[i]))
 
 leaderNumber = int(input("Which leader would you like to choose? "))
 Player = Leader(leaders[leaderNumber-1])
@@ -45,6 +49,6 @@ print("The units available for your leader are:\n" )
 
 for i in range(3): print(str(i+1)+")", "{0}".format(Player.getUnit(Player.leader, i)))
 
-
+weatherDecision = random.randint(1,3)
 
 
